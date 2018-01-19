d895a58f9d0659b6b0d48a89fe07b200b5fc08a5
Israel Navarrete <navarrete.alvarado@gmail.com>
2018-01-18 20:12:11 -0500

    2018-01-18

--- core/Router.php
+++ core/Router.php
@@ -28,6 +28,10 @@ class Router {
 		}
 		$controller_name = $controller;
 		$controller = ucwords($controller);
+		if (DEBUG) {
+			$action=(isset($action))?$action:'';
+			error_log($url.'=>'.$controller_name.'/'.$action); 
+		}	
 		if (class_exists($controller) and method_exists($controller, $action)) {
 			if (DEBUG) {
 				$action=(isset($action))?$action:'';
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,14 +4,11 @@ require_once('core' . DS . 'functions.php');
 function autoload($className) {
 	if (file_exists(ROOT . DS .'core' . DS . ($className) . '.php')) {
         require_once(ROOT . DS .'core' . DS . ($className) . '.php');
-    }
-	else if (file_exists(ROOT . DS .'application' . DS . 'controllers' . DS . ($className) . '.php')) {
+    }else if (file_exists(ROOT . DS .'application' . DS . 'controllers' . DS . ($className) . '.php')) {
         require_once(ROOT . DS .'application' . DS . 'controllers' . DS . ($className) . '.php');
-    }
-	else if (file_exists(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php')) {
+    }else if (file_exists(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php')) {
         require_once(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php');
-	}
-	else if (file_exists(ROOT . DS . 'application' . DS . 'helpers' . DS . $className . '.php')) {
+	}else if (file_exists(ROOT . DS . 'application' . DS . 'helpers' . DS . $className . '.php')) {
         require_once(ROOT . DS . 'application' . DS . 'helpers' . DS . $className . '.php');
 	}
 }
